6df04183d5a979928732d993de343197e903deb9
Jas0n2k <bnn8217483@gmail.com>
2020-08-23 20:43:43 +0800

    Huge Updates.

---
+++ js/clipboard.js
@@ -0,0 +1,17 @@
+var cbData = event.clipboardData || window.clipboardData
+document.getElementById('uid').addEventListener('paste',function(event){
+
+    if(!cbData){
+        var rexp = '/'
+    }
+    else {
+
+    }
+})
+
+const paste = function(){
+    if(!cbData){
+        
+    }
+    
+}
