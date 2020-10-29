aa71ee1e5162d331697b340ddca8506b0ced99b9
Jas0n2k <bnn8217483@gmail.com>
2020-10-29 18:23:24 +0800

    Sync changes for another device.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 var $$ = mdui.$
-$$(function(){
+$$(function () {
     let $form = $$('form')
-    $$('#submit').on('click', function(){
-        if($form)
-        $form[0].submit()
+     
+    $$('#submit').on('click', function () {
+        if ($form[0].checkValidity()) {
+            $form[0].submit()
+        }
     })
+
+    const appendScript = function (father) {
+        let url = "https://jsonp.afeld.me/?callback=showFansCount&url=https%3A%2F%2Fapi.bilibili.com%2Fx%2Fweb-interface%2Fcard%3Fmid%3D" + userID + '&spam=' + Number(new Date)
+        let elScriptChild = document.createElement('script')
+        elScriptChild.setAttribute('src', url)
+        elScriptChild.setAttribute('type', "text/javascript")
+        elScriptChild.setAttribute('referrerpolicy', "no-referrer")
+        father.appendChild(elScriptChild)
+    }
 })
