5f0def25f96aa329f0344b9f5c54756162ff920a
Jas0n2k <bnn8217483@gmail.com>
2020-11-02 19:46:47 +0800

    Huge update.
    1. Add verification for entry page.
    2. Add param verification for counter renderer.
    3. Reconstruct codes.

--- js/UIDquery.js
+++ js/UIDquery.js
@@ -1,28 +1,31 @@
-window.onload = function () {
-    var userID, duration = 3000;
-    elAvatar = document.getElementById('avatar')
-    elFans = document.getElementById('fans')
-    elScript = document.getElementById('script')
-    elName = document.getElementById('name')
+var userID, duration = 3000;
+(function () {
+    let handleURL = new URLSearchParams(window.location.search)
+    if (handleURL.has('uid')) {
 
-    function handle() {
-        let handleURL = new URLSearchParams(window.location.search)
-        if (handleURL.has('uid'))
+        if (/[0-9]{1,11}$/.test(handleURL.get('uid'))) {
             userID = handleURL.get('uid');
+        }
         else {
-            window.alert("No UID, set default.");
-            userID = 262453663;
+            window.alert("Param is not legit.");
+            window.location.href = 'index.html';
         }
     }
-    handle();
+    else {
+        window.alert("No UID.");
+        window.location.href = 'index.html';
+    }
+})();
 
-    const appendScript = function (father) {
+window.onload = function () {
+    elScript = document.getElementById('script')
+    const appendScript = function () {
         let url = "https://jsonp.afeld.me/?callback=showFansCount&url=https%3A%2F%2Fapi.bilibili.com%2Fx%2Fweb-interface%2Fcard%3Fmid%3D" + userID + '&spam=' + Number(new Date)
         let elScriptChild = document.createElement('script')
         elScriptChild.setAttribute('src', url)
         elScriptChild.setAttribute('type', "text/javascript")
         elScriptChild.setAttribute('referrerpolicy', "no-referrer")
-        father.appendChild(elScriptChild)
+        elScript.appendChild(elScriptChild)
     }
 
     const subodometer = document.querySelector(".myOdometer");
@@ -33,9 +36,9 @@ window.onload = function () {
         duration: 200
     })
 
-    appendScript(elScript);
-    window.setInterval(function(){
+    appendScript();
+    window.setInterval(function () {
         elScript.removeChild(elScript.childNodes[0])
-        appendScript(elScript)
+        appendScript()
     }, duration);
 }
--- js/index.js
+++ js/index.js
@@ -1,11 +1,44 @@
 var $$ = mdui.$
 $$(function () {
-    let $form = $$('form')
-     
-    $$('#submit').on('click', function () {
-        if ($form[0].checkValidity()) {
-            $form[0].submit()
+    let $form = $$('form');
+    let $IDField = $$('#IDfield')
+    let $uid = $$('#uid');
+    let $submit = $$('#submit');
+    let $errorInfo = $$('#errorInfo');
+    var re = /^[0-9]{1,11}$/;
+    var re2 = /.*space.bilibili.com\/[0-9]{1,11}\/?$/;
+    var re3 = /[0-9]{1,11}$/
+
+    $uid.on('keydown', function (e) {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            $submit.trigger('click')
         }
+    }).on('input', function () {
+        $IDField.removeClass('mdui-textfield-invalid');
+    })
+
+    $submit.on('click', function () {
+        let pass = false
+        let input = $uid.val()
+        if (input) {
+            if (re.test(input) || re2.test(input)) {
+                $uid.val(input.match(re3)[0]);
+                pass = true;
+            }
+            else {
+                $errorInfo.text('输入不合法');
+                $IDField.addClass('mdui-textfield-invalid');
+            }
+        }
+        else {
+            $errorInfo.text('输入值不能为空');
+            $IDField.addClass('mdui-textfield-invalid');
+        }
+        if (pass) {
+            $form[0].submit();
+        }
+
     })
 
     const appendScript = function (father) {
