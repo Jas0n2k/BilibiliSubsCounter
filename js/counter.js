498313fa9145cd00331290ae844f7a9f8dc591f2
Jas0n2K <bnn8217483@gmail.com>
2021-08-02 21:35:04 +0800

    [Version 1.0] 1. Refactor. Using more CSS to make it look better. 2. Use fetchAPI to update subscriber count. 3. Support manually and auto pausing updating. 4. Use CDN that is much faster (thanks to jsdelivr.com).

--- js/counter.js
+++ js/counter.js
@@ -1,3 +1,23 @@
+const avatar = document.getElementById("c-avatar");
+const nickname = document.getElementById("c-nickname");
+const odometer = document.getElementById("odometer");
+const titleBar = document.getElementById("titleBar");
+const overlayInitDialogCB = document.getElementById("overlayInitDialogCB")
+const overlaySettingsCB = document.getElementById("overlaySettingsCB");
+const goBtn = document.getElementById("goBtn");
+const updateToggle = document.getElementById("updateToggle");
+const reduceMotionTg = document.getElementById("reduceMotion");
+const flag = {
+    keepUpdating: () => updateToggle.checked,
+    isPaused: () => !(overlaySettingsCB.checked),
+    isVisible: true
+};
+let userID;
+let handleURL = new URLSearchParams(window.location.search)
+let updateInterval = 3000;
+let retryTimes = 2;
+
+
 const JSONP_API = [
     "https://jsonp.afeld.me/?url=",
     "https://api.codetabs.com/v1/proxy/?quest="
@@ -21,7 +41,7 @@ const fetchWithTimeout = (url, options, timeout = 5000) => new Promise((resolve,
         })
 })
 
-const getInfo = (UID) => new Promise((resolve, reject) => {
+const getInfo = UID => new Promise((resolve, reject) => {
     fetchWithTimeout(JSONP_API[1] + bilibiliAPI(UID), {
         method: "GET",
         referrerPolicy: "no-referrer"
@@ -29,18 +49,18 @@ const getInfo = (UID) => new Promise((resolve, reject) => {
         .then(res => {
             if (res.ok) {
                 return res.json();
-            } else throw new Error("Not OK!")
+            } else throw new Error("Fetch Result is Not OK!")
         })
-        .then(json => {
-            if (Number(json.code) !== 0) {
-                let customErr = new Error(json.message);
-                customErr.code = json.code;
+        .then(res => {
+            if (Number(res.code) !== 0) {
+                let customErr = new Error(res.message);
+                customErr.code = res.code;
                 throw customErr;
             } else {
                 resolve({
-                    avatarURL: json.data.card.face,
-                    nickname: json.data.card.name,
-                    follower: json.data.follower
+                    avatarURL: res["data"]["card"]["face"],
+                    nickname: res["data"]["card"]["name"],
+                    follower: res["data"]["follower"]
                 })
             }
         })
@@ -52,12 +72,14 @@ const getInfoWithRetry = (UID, retryTimes = 3) => new Promise(((resolve, reject)
         getInfo(UID)
             .then(resolve)
             .catch(e => {
-                console.log(`Failed. Attempts left: ${retryTimes}`);
-                if (!retryTimes) {
-                    reject(e)
-                } else {
-                    retryTimes--;
-                    setTimeout(attempt, 2000)
+                if (e.message === "UID NOT LEGIT") reject(e)
+                else {
+                    console.log(`Failed. Attempts left: ${retryTimes}`);
+                    if (!retryTimes) reject(e)
+                    else {
+                        retryTimes--;
+                        setTimeout(attempt, 2000)
+                    }
                 }
             })
     }
@@ -65,68 +87,54 @@ const getInfoWithRetry = (UID, retryTimes = 3) => new Promise(((resolve, reject)
     attempt();
 }))
 
-window.onload = () => {
-    const avatar = document.getElementById("c-avatar");
-    const nickname = document.getElementById("c-nickname");
-    const odometer = document.getElementById("odometer");
-    const titleBar = document.getElementById("titleBar");
-    const overlayCB = document.getElementById("overlayCB")
-    const updateToggle = document.getElementById("updateToggle");
-    const reduceMotionTg = document.getElementById("reduceMotion");
-    let updateInterval = 3000;
-    let retryTimes = 2;
-    let userID = 258150656;
+userID = (handleURL.has('uid') && /[1-9][0-9]{0,10}$/.test(handleURL.get('uid'))) ? handleURL.get('uid') : "";
 
-    const flag = {
-        keepUpdating: () => updateToggle.checked,
-        isPaused: () => !(overlayCB.checked),
-        isVisible: true
-    }
+const init = (_userID, _retryTimes) => {
+    if (_userID.length === 0) {
+        console.log("EMPTY UID")
+        return overlayInitDialogCB.checked = false;
+    } else if (isNaN(_userID)) return console.error("UID NOT LEGIT")
 
-    reduceMotionTg.addEventListener("change", e => {
-        if (e.target.checked) document.body.classList.add("no-motion");
-        else document.body.classList.remove("no-motion");
-    })
-
-    titleBar.addEventListener("click", () => overlayCB.checked = false)
-
-    document.addEventListener("visibilitychange", () => {
-        flag.isVisible = document.visibilityState !== 'hidden';
-    })
-
-
-    const init = (_userID, _retryTimes) => new Promise((resolve, reject) => {
-        getInfoWithRetry(_userID, _retryTimes)
-            .then(res => {
-                odometer.classList.remove("loading");
-                avatar.src = res.avatarURL;
-                nickname.innerText = res.nickname;
-                odometer.innerText = res.follower;
-                document.title = `${res.nickname} - Bilibili 实时粉丝计数器`
-                resolve()
-            })
-            .catch(e => {
-                console.log(e)
-                reject(e)
-            })
-    })
-
-    const updatePeriodically = () => {
-        if (flag.keepUpdating() && !flag.isPaused()) {
-            getInfoWithRetry(userID, retryTimes)
-                .then(res => odometer.innerText = res.follower)
-                .catch(e => {
-                    console.log(e);
-                    updateToggle.checked = false;
-                })
-                .finally(() => setTimeout(updatePeriodically, updateInterval))
-        } else setTimeout(updatePeriodically, 1200)
-
-    }
-
-    init(userID, retryTimes)
+    getInfoWithRetry(_userID, _retryTimes)
+        .then(res => {
+            odometer.classList.remove("loading");
+            avatar.src = res.avatarURL;
+            nickname.innerText = res.nickname;
+            odometer.innerText = res.follower;
+            document.title = `${res.nickname} - Bilibili 实时粉丝计数器`
+        })
         .then(() => setTimeout(updatePeriodically, updateInterval))
         .catch(e => {
             console.log(e);
         })
 }
+
+const updatePeriodically = () => {
+    if (flag.keepUpdating() && !flag.isPaused()) {
+        getInfoWithRetry(userID, retryTimes)
+            .then(res => odometer.innerText = res.follower)
+            .catch(e => {
+                console.log(e);
+                updateToggle.checked = false;
+            })
+            .finally(() => setTimeout(updatePeriodically, updateInterval))
+    } else setTimeout(updatePeriodically, 1200)
+
+}
+
+reduceMotionTg.addEventListener("change", e => {
+    if (e.target.checked) document.body.classList.add("no-motion");
+    else document.body.classList.remove("no-motion");
+})
+
+titleBar.addEventListener("click", () => overlaySettingsCB.checked = false)
+
+document.addEventListener("visibilitychange", () => flag.isVisible = document.visibilityState !== 'hidden')
+
+goBtn.addEventListener("click", () => {
+    userID = document.getElementById("UIDInputBox").value;
+    overlayInitDialogCB.checked = true;
+    init(userID, retryTimes)
+})
+
+init(userID, retryTimes);
